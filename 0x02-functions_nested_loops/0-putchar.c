a977fa757c0cdfa9e5ec0ee715bee4fce6e51e0b
Baron <afutubaronny@gmail.com>
2023-09-13 22:33:58 -0700

    Tasks 2 to 6

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,10 @@
 #include <unistd.h>
 #include "main.h"
+
 /**
- * File Description
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
  */
 int main(void)
 {
